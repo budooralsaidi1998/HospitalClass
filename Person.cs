cabf356ab4ca5d2f31dad6629dad02ad7f6cf173
budoor <budooralsaidi1998@gmail.com>
2024-09-29 15:27:28 +0400

    add the ttribute and their method

---
+++ Person.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace HospitalClassINhernite
+{
+    public  class Person
+    {
+
+        public string Name;
+        public int Age;
+        public string gender;
+
+        public Person(string name, int age, string Gender)
+        {
+            Name = name;
+            Age = age;
+            if (Gender == "femal")
+            {
+             
+                gender = Gender;
+            }
+            else
+            {
+                gender = Gender;
+            }
+
+
+
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name : {Name} , Age : {Age} , Gender : {gender}");
+        }
+
+
+
+
+
+
+
+    }
+}
