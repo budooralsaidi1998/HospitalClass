766cdb6531f3b4cbfc2445b4d033b818e8c58ba7
budoor <budooralsaidi1998@gmail.com>
2024-09-30 10:57:21 +0400

    add funtions for room

--- Patient.cs
+++ Patient.cs
@@ -4,16 +4,19 @@ namespace HospitalClassINhernite
 {
     public class Patient:Person
     {
+        List<Doctor>doctors = new List<Doctor>();
         public int PatientID;
         public string Ailment;
         public string AssignedDoctor;
         public int Room = 0;
-        public Patient(string name, int age, string Gender,int pationid,string ailmenrt,string assigndoctor, int room):base(name, age, Gender)
+        
+        public Patient(int pationid,string name, int age, string Gender,string ailmenrt,string assigndoctor):base(name, age, Gender)
         {
             PatientID = pationid;
             Ailment = ailmenrt;
+            //id of doctor
             AssignedDoctor = assigndoctor;
-            Room = room;
+           
             
 
         }
---
+++ Room.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace HospitalClassINhernite
+{
+    public class Room
+    {
+        public static List<Patient> roomlist = new List<Patient>();
+        public int RoomNumber;
+        public string RoomType;
+        public bool IsOccupied = true;
+
+        public Room(int roomNumber, string roomType, bool isOccupied)
+        {
+            RoomNumber = roomNumber;
+            RoomType = roomType;
+            IsOccupied = isOccupied;
+        }
+
+
+        public void OccupyRoom()
+        {
+            for (int i = 0; i < roomlist.Count; i++)
+            {
+                if (roomlist[i].Room == RoomNumber)
+                {
+                    Console.WriteLine($"Room {RoomNumber} is occupied: {IsOccupied}");
+                }
+                else
+                {
+                    Console.WriteLine($"Room {RoomNumber} is occupied: {!IsOccupied}");
+                }
+            }
+               
+        }
+        public void VacateRoom()
+        {
+            for (int i = 0; i < roomlist.Count; i++)
+            {
+                if (roomlist[i].Room == null)
+                
+                {
+                    Console.WriteLine($"Room {RoomNumber} is available");
+                }
+            }
+
+        }
+    }
+}
