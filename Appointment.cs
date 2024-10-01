a67f9ff04359e7f95bb442815e001cbd59966ac7
budoor <budooralsaidi1998@gmail.com>
2024-10-01 08:56:31 +0400

    add function and date appoinat

---
+++ Appointment.cs
@@ -0,0 +1,40 @@
+﻿
+namespace HospitalClassINhernite
+{
+    public class Appointment
+    {
+
+       public Patient patient;
+        public Doctor doctor;
+        public DateTime ?AppointmentDate;
+
+        public Appointment(Patient patient, Doctor doctor,DateTime dateappo)
+        {
+            this.patient = patient;
+            this.doctor = doctor;
+            AppointmentDate=dateappo;
+
+        }
+
+        public void ScheduleAppointment(DateTime dateappo)
+        {
+            AppointmentDate = dateappo;
+        }
+
+
+        public void CancelAppointment()
+        {
+            AppointmentDate = null;
+        }
+
+
+        public void GetAppointmentDetails()
+        {
+            Console.WriteLine($"Appointment scheduled for {patient.Name} with {doctor.Name} on {AppointmentDate}");
+        }
+
+
+
+
+    }
+}
