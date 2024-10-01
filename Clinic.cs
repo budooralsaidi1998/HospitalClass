88686bf8b36e3a6851510d213f96bc41fa1171a8
budoor <budooralsidi1998@gmail.com>
2024-10-01 11:20:19 +0400

    add attribut clinic

--- Clinic.cs
+++ Clinic.cs
@@ -8,5 +8,20 @@ namespace HospitalClassINhernite
 {
     public class Clinic
     {
+
+        public int ClinicID;
+        public string ClinicName;
+        public enum Specialization { Cardiology, Neurology, Dermatology };
+
+        public Specialization spClinic;
+
+        public static List<Room> rooms = new List<Room>();
+
+        public Dictionary<Doctor, List<Appointment>> AvailableAppointments= new Dictionary<Doctor, List<Appointment>>();
+ 
+
+
+
+
     }
 }
