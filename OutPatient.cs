9d7c8acbf3a3b9959fd3007a7533b1c44db8fb3a
budoor <budooralsidi1998@gmail.com>
2024-10-01 15:54:59 +0400

    add function display info for upcoming

---
+++ OutPatient.cs
@@ -0,0 +1,33 @@
+﻿
+namespace HospitalClassINhernite
+{
+    public  class OutPatient:Patient
+    {
+       public  Clinic ClinicAssigned;
+     
+            
+    
+        public OutPatient(int pationid, string name, int age, Gender gender, string ailmenrt,Doctor assigndoctor,Clinic clinicAssigned):base(pationid, name, age, gender, ailmenrt, assigndoctor)
+        {
+            ClinicAssigned = clinicAssigned;
+       
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("Upcoming Appointments:");
+            foreach (var appointment in ClinicAssigned.AvailableAppointments)
+            {
+                foreach (var item in appointment.Value)
+                {
+                    if (item.AppointmentDate > DateTime.Now)
+                    {
+                        Console.WriteLine($"Date: {item.AppointmentDate}, Time: {item.AppointmentTime}, IsBooked: {item.IsBooked}");
+                    }
+                }
+            }
+        }
+    }
+
+}
