4c4759e6b32f6598ffa1f7be0d3bc0e9143cafda
budoor <budooralsidi1998@gmail.com>
2024-10-08 10:09:27 +0400

    add interface to manage the room

---
+++ IRoomManagment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalClassINhernite
+{
+    public  interface IRoomManagment
+    {
+        void OccupyRoom();
+        void VacateRoom();
+        void DisplayRoomInfo();
+    }
+}
